cec835c36a473ebd5727d52dc72b95a1bb288eb6
Lincoln Adams <lincolnadams@icloud.com>
2025-12-04 23:37:55 -0700

    Fixing knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,8 +6,8 @@ module.exports = {
         connection: {
             host: 'localhost',
             user: 'postgres',
-            password: 'BYUPassword',
-            database: 'intex',
+            password: 'postgres',
+            database: 'admin',
             port: 5432,
         },
         migrations: {
